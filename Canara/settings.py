5db34c2cba35f16d12524b2aa86b3304d4d3b061
Chandresh-spec <chandumoger819@gmail.com>
2025-10-19 22:27:25 +0530

    added teacher section

--- Canara/settings.py
+++ Canara/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'canaradatabase',
     'Course',
+    'teachers',
 ]
 
 MIDDLEWARE = [
--- Course/views.py
+++ Course/views.py
@@ -5,3 +5,9 @@ from .models import Courses
 def BCA_views(request):
     bca_courses=Courses.objects.filter(course_name='BCA')
     return render(request,'bca.html',{'bca_courses':bca_courses})
+
+
+
+def BBA_Views(request):
+    bba_course=Courses.objects.filter(course_name='BBA')
+    return render(request,'bba.html',{'bba_course':bba_course})
